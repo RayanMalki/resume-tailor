81baf78cc3fdc28c84d335c26c7cb4b131d97fd8
RayanMalki <rayanmalki54@gmail.com>
2025-12-17 17:35:24 -0500

    added cookie creation, cookie reading and cookie deletion

---
+++ backend/internal/httpapi/session_cookie.go
@@ -0,0 +1,54 @@
+package httpapi
+
+import (
+	"net/http"
+	"time"
+)
+
+const SessionCookieName = "session"
+
+func SetSessionCookie(w http.ResponseWriter, token string, expiresAt time.Time) {
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     SessionCookieName,
+		Value:    token,
+		Expires:  expiresAt,
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   false, //for local dev set to false or browser will refuse the cookie
+		SameSite: http.SameSiteLaxMode,
+	})
+
+}
+
+func ReadSessionCookie(r *http.Request) (token string, ok bool) {
+
+	cookie, err := r.Cookie(SessionCookieName)
+
+	if err != nil {
+		return "", false
+
+	}
+
+	if cookie.Value == "" {
+		return "", false
+
+	}
+	return cookie.Value, true
+
+}
+
+func ClearSessionCookie(w http.ResponseWriter) {
+	c := &http.Cookie{
+		Name:     SessionCookieName,
+		Value:    "",
+		Path:     "/",
+		Expires:  time.Unix(0, 0),
+		MaxAge:   -1,
+		HttpOnly: true,
+		Secure:   false, //for local dev set to false or browser will refuse the cookie
+		SameSite: http.SameSiteLaxMode,
+	}
+
+	http.SetCookie(w, c)
+}
