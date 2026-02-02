2332333d4df75a30629ea39fc5f95ef86a45b85c
RayanMalki <rayanmalki54@gmail.com>
2026-02-02 16:32:39 -0500

    Move run status backfill to new migration
    Removed run status update statements from 003_run_status_update.sql and added them to a new migration 005_run_status_backfill.sql. This separates schema changes from data backfill for better migration management.

--- backend/migrations/005_run_status_backfill.sql
+++ backend/migrations/005_run_status_backfill.sql
@@ -1,17 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
 
-DO $$ BEGIN
-  ALTER TYPE run_status ADD VALUE IF NOT EXISTS 'running';
-EXCEPTION
-  WHEN duplicate_object THEN NULL;
-END $$;
-
-DO $$ BEGIN
-  ALTER TYPE run_status ADD VALUE IF NOT EXISTS 'succeeded';
-EXCEPTION
-  WHEN duplicate_object THEN NULL;
-END $$;
+UPDATE runs SET status = 'queued' WHERE status = 'created';
+UPDATE runs SET status = 'running' WHERE status = 'processing';
+UPDATE runs SET status = 'succeeded' WHERE status = 'completed';
 
 -- +goose StatementEnd
 
@@ -21,7 +13,5 @@ END $$;
 UPDATE runs SET status = 'completed' WHERE status = 'succeeded';
 UPDATE runs SET status = 'processing' WHERE status = 'running';
 UPDATE runs SET status = 'created' WHERE status = 'queued';
-
--- NOTE: We don't remove enum values in down migration.
 
 -- +goose StatementEnd
